f110c473a98cff6f074b9cbdff22dba1d2acb08a
Clemens-O. Hoppe <coh@co-hoppe.net>
2012-09-12 17:16:24 +0200

    numerous PEP8 fixes. removed tracked .pyc files added .gitignore

--- judge.py
+++ judge.py
@@ -1,4 +1,5 @@
-__description__ = 'Analyzes PDF files and classifies them based on features present'
+__description__ = \
+    'Analyzes PDF files and classifies them based on features present'
 __author__ = 'Brandon Dixon'
 __version__ = '1.0'
 __date__ = '2012/04/28'
@@ -10,9 +11,11 @@ from classyPDF import *
 from peepdf_classy.bsdPDFCore import PDFParser
 import optparse
 
+
 mal = Orange.data.Table("data/complete.tab")
 learner = Orange.classification.knn.kNNLearner()
 classifier = learner(mal)
+
 
 def decision(d):
     c = []
@@ -21,8 +24,8 @@ def decision(d):
     t = ""
 
     try:
-        tmp = Orange.data.Instance(mal.domain,d)
-        c = classifier(tmp,Orange.classification.Classifier.GetBoth)
+        tmp = Orange.data.Instance(mal.domain, d)
+        c = classifier(tmp, Orange.classification.Classifier.GetBoth)
         g = int(math.ceil(c[1][0] * 100))
         b = int(c[1][1] * 100)
         t = int(c[1][2] * 100)
@@ -32,31 +35,38 @@ def decision(d):
         c.append(666)
 
     if c[0] == 0:
-        des = "non-malicious\t(G:{0}%\tB:{1}%\tT:{2}%)".format(g,b,t)
+        des = "non-malicious\t(G:{0}%\tB:{1}%\tT:{2}%)".format(g, b, t)
     elif c[0] == 1:
-        des = "malicious\t\t(G:{0}%\tB:{1}%\tT:{2}%)".format(g,b,t)
+        des = "malicious\t\t(G:{0}%\tB:{1}%\tT:{2}%)".format(g, b, t)
     elif c[0] == 2:
-        des = "targeted\t\t(G:{0}%\tB:{1}%\tT:{2}%)".format(g,b,t)
+        des = "targeted\t\t(G:{0}%\tB:{1}%\tT:{2}%)".format(g, b, t)
     else:
         des = "failed"
 
     return des
-    
+
+
 def handle_file(f):
     pdfParser = PDFParser()
-    ret,pdf = pdfParser.parse(f, True, False)
-    obj = FullMetalExtractor(pdf,"_")
+    ret, pdf = pdfParser.parse(f, True, False)
+    obj = FullMetalExtractor(pdf, "_")
     tab = obj.getTab().split("\t")
     des = decision(tab)
     return des
-    
+
+
 def main():
-    oParser = optparse.OptionParser(usage='usage: %prog [options]\n' + __description__, version='%prog ' + __version__)
-    oParser.add_option('-f', '--file', default='', type='string', help='file to build an object from')
-    oParser.add_option('-d', '--dir', default='', type='string', help='dir to build an object from')
-    oParser.add_option('-r', '--report', default='', type='string', help='create basic report')
+    oParser = optparse.OptionParser(usage='usage: %prog [options]\n' +
+                                    __description__, version='%prog ' +
+                                    __version__)
+    oParser.add_option('-f', '--file', default='', type='string',
+                       help='file to build an object from')
+    oParser.add_option('-d', '--dir', default='', type='string',
+                       help='dir to build an object from')
+    oParser.add_option('-r', '--report', default='', type='string',
+                       help='create basic report')
     (options, args) = oParser.parse_args()
-    
+
     if options.file:
         decision = handle_file(options.file)
         print options.file + " : " + decision
@@ -71,8 +81,8 @@ def main():
             decision = handle_file(options.dir + file)
             print decision + " : " + file
     else:
-        oParser.print_help()	
-        return
-                                
+        return oParser.print_help()
+
+
 if __name__ == '__main__':
     main()
